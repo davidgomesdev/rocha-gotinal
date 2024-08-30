c4f977aa63f8f2db84cf29aab98e773795edcd7c
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2024-08-30 21:22:45 +0100

    Feat/add workflows (#1)
    * chore: format code
    
    * ci: add build and release workflow
    
    ---------
    
    Co-authored-by: David Gomes <david.gomes@daimlertruck.com>

--- discord_sender.go
+++ discord_sender.go
@@ -13,27 +13,27 @@ const username = "Fernando Rocha"
 const avatarUrl = "https://i.ibb.co/QHHPPH4/roch1.jpg"
 
 type WebhookPayload struct {
-	Username string
+	Username   string
 	Avatar_url string
 }
 
 func SendClip(clip Clip, webhookUrl string) {
 	file, _ := os.Open(clip.filePath)
-  defer file.Close()
+	defer file.Close()
 
 	body := &bytes.Buffer{}
-  
+
 	writer := multipart.NewWriter(body)
-  part, _ := writer.CreateFormFile("files[0]", clip.name)
-  io.Copy(part, file)
-  writer.Close()
+	part, _ := writer.CreateFormFile("files[0]", clip.name)
+	io.Copy(part, file)
+	writer.Close()
 
 	payload, _ := json.Marshal(WebhookPayload{username, avatarUrl})
 
 	writer.WriteField("payload_json", string(payload))
 
-  r, _ := http.NewRequest("POST", webhookUrl, body)
-  r.Header.Add("Content-Type", writer.FormDataContentType())
-  client := &http.Client{}
-  client.Do(r)
+	r, _ := http.NewRequest("POST", webhookUrl, body)
+	r.Header.Add("Content-Type", writer.FormDataContentType())
+	client := &http.Client{}
+	client.Do(r)
 }
