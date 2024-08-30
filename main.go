d4b874542f993d7e87d763a2b7779432eba7e5d0
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2024-08-30 21:22:44 +0100

    chore: get clips

--- main.go
+++ main.go
@@ -2,37 +2,58 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
-	"runtime/debug"
+	"regexp"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	"golang.org/x/text/unicode/norm"
 )
 
 const fetchSize = 100
 
+const extract_clip_name_regex = "\\d\\d\\. (.+?) -.+\\.mp3"
+const sanitize_regex = "[^a-zA-Z0-9 -.]+"
+
 func main() {
 	token := os.Getenv("BOT_TOKEN")
 	channelId := os.Getenv("CHANNEL_ID")
+
+	if token == "" || channelId == "" {
+		log.Fatal("You need to provide BOT_TOKEN and CHANNEL_ID environment variables.")
+	}
+
 	discord, err := discordgo.New(fmt.Sprint("Bot ", token))
 	exitErr(err)
 
 	msgs, err := discord.ChannelMessages(channelId, fetchSize, "", "", "")
 	exitErr(err)
 
-	totalMsgs := make([]discordgo.Message, len(msgs))
+	sentClips := getSentClips(msgs, discord, channelId)
+
+	clips, err := os.ReadDir("clips/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, clip := range clips {
+		log.Println("normalized", normalizeClipName(clip.Name()))
+	}
+
+	log.Println("---------------------------")
+	log.Println("Got", len(sentClips), "messages")
+}
+
+func getSentClips(msgs []*discordgo.Message, discord *discordgo.Session, channelId string) []string {
+	var totalClipsSent []string
 
 	for ok := true; ok; ok = len(msgs) > 0 {
-		for i := range msgs {
-			msg := msgs[i]
-
-			if len(msg.Attachments) > 0 {
-				fmt.Println(msg.Attachments[0].Filename)
-			}
-		}
-
-		for item := range msgs {
-			if msgs[item] != nil {
-				totalMsgs = append(totalMsgs, *msgs[item])
+		for _, msg := range msgs {
+			if msg != nil && len(msg.Attachments) > 0 {
+				if fileName := msg.Attachments[0].Filename; fileName != "" {
+					totalClipsSent = append(totalClipsSent, msg.Attachments[0].Filename)
+				}
 			}
 		}
 
@@ -41,20 +62,29 @@ func main() {
 		msgs = newMsgs
 	}
 
-	for _, msg := range totalMsgs {
-		if len(msg.Attachments) > 0 {
-			fmt.Println(msg.Attachments[0].Filename)
-		}
+	return totalClipsSent
+}
+
+func normalizeClipName(originalText string) string {
+	var text = norm.NFC.String(originalText)
+
+	var re = regexp.MustCompile(extract_clip_name_regex)
+
+	match := re.FindStringSubmatch(originalText)
+
+	if len(match) == 1 {
+		return ""
 	}
 
-	fmt.Println("---------------------------")
-	fmt.Println(fmt.Sprint("Got ", len(totalMsgs), " messages"))
+	text = match[1]
+	re = regexp.MustCompile(sanitize_regex)
+	text = re.ReplaceAllString(text, "")
+	
+	return strings.ReplaceAll(strings.TrimSpace(text), " ", "_")
 }
 
 func exitErr(err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		debug.PrintStack()
-		os.Exit(1)
+		log.Fatal(err)
 	}
 }
